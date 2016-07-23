d4f0a2b5b1462a60df8ff9c781052ea6eef741eb
Oguzhan Gungor <ocg@aslan-apps.com>
2016-07-23 21:39:51 +1000

    init

---
+++ OCGMergeSort/OCGMergeSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCGMergeSort
+//
+//  Created by Oguzhan Gungor on 23/07/2016.
+//  Copyright © 2016 Aslan-Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCGMergeSort/OCGMergeSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCGMergeSort
+//
+//  Created by Oguzhan Gungor on 23/07/2016.
+//  Copyright © 2016 Aslan-Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
